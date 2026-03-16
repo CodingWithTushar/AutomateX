2a60ebcabddf6e971935504af91e50d426ad369c
CodingWithTushar <156307843+CodingWithTushar@users.noreply.github.com>
2026-03-16 10:11:07 +0530

    meeting-page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -21,8 +21,13 @@ export const meetingRouter = createTRPCRouter({
       const [existingMeetings] = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+            "duration",
+          ),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)),
         );
@@ -103,6 +108,7 @@ export const meetingRouter = createTRPCRouter({
         totalPages,
       };
     }),
+
   create: protectedProcedure
     .input(MeetingInsertSchema)
     .mutation(async ({ input, ctx }) => {
@@ -135,5 +141,28 @@ export const meetingRouter = createTRPCRouter({
       }
 
       return updateMeeting;
+    }),
+  remove: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const [removeMeeting] = await db
+        .delete(meetings)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)),
+        )
+        .returning();
+
+      if (!removeMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return removeMeeting;
     }),
 });
--- src/modules/meetings/ui/components/update-meeting-dialog.tsx
+++ src/modules/meetings/ui/components/update-meeting-dialog.tsx
@@ -1,37 +1,34 @@
 "use client";
-import { useRouter } from "next/navigation";
 
 import { CommandResponsiveDialog } from "@/components/ui/command";
 import MeetingForm from "./meeting-form";
+import { GetOneMeetingType } from "../../types";
 
 interface Props {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  initialValues: GetOneMeetingType;
 }
 
-const NewMeetingDialog = ({ open, onOpenChange }: Props) => {
-  const router = useRouter();
-
+const UpdateMeetingDialog = ({ open, onOpenChange, initialValues }: Props) => {
   return (
     <>
       <CommandResponsiveDialog open={open} onOpenChange={onOpenChange}>
         <div className="flex flex-col px-4 pt-4 gap-1">
           <h4>
             {" "}
-            <b>New Meeting</b>
+            <b>Edit Meeting</b>
           </h4>
-          <h6>Create new Ai meeting</h6>
+          <h6>Edit the meeting details</h6>
         </div>
         <MeetingForm
-          onSuccess={(id) => {
-            onOpenChange(false);
-            router.push(`/meetings/${id}`);
-          }}
+          onSuccess={() => onOpenChange(false)}
           onCancel={() => onOpenChange(false)}
+          initialValues={initialValues}
         />
       </CommandResponsiveDialog>
     </>
   );
 };
 
-export default NewMeetingDialog;
+export default UpdateMeetingDialog;
